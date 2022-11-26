2d46b29be45a06c8087d73375aca9386874918de
Karn131059 <90178303+Karn131059@users.noreply.github.com>
2022-11-26 19:43:06 +0700

    Add files via upload

---
+++ PickItem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PickItem : MonoBehaviour
+{
+    private int score = 0;
+    public Text scoreText;
+    private AudioSource audioSource;
+    public AudioClip itemSound;
+   
+
+    int itemCount;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        itemCount = GameObject.FindGameObjectsWithTag("Item").Length;
+        scoreText.text = "Item =  " + score.ToString() + "/" + itemCount.ToString();
+        
+    }
+    private void OnTriggerEnter(Collider target)
+    {
+        if (target.gameObject.tag.Equals("Item"))
+        {
+            Destroy(target.gameObject);
+
+            score += 1;
+            scoreText.text = "Item =  " + score.ToString() + "/" + itemCount.ToString();
+            audioSource.PlayOneShot(itemSound);
+        }
+    }
+    
+
+}
